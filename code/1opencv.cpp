fb641da4aa1871707af0ca2df151f4416e9a512d
AlexeyVolchenkov <72989930+AlexeyVolchenkov@users.noreply.github.com>
2022-07-18 15:22:26 +0300

    Add files via upload

--- code/1opencv.cpp
+++ code/1opencv.cpp
@@ -3,6 +3,9 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace cv;
 using namespace std;
@@ -19,13 +22,20 @@ private:
 	Scalar min, max;
 	Mat mask;
 	RotatedRect rot_rect;
-	float x, y;
 	string string_x, string_y, coord;
+	Moments oMoments;
+	double dM01;
+	double dM10;
+	double dArea;
 public:
+	float min_dist = 1000;
+	string name;
+	int x, y;
 	Point center;
 	vector<vector<Point>> contours;
-	Markers(Scalar mincolor, Scalar maxcolor)
+	Markers(Scalar mincolor, Scalar maxcolor, string name_color)
 	{
+		name = name_color;
 		min = mincolor;
 		max = maxcolor;
 	}
@@ -35,32 +45,48 @@ public:
 	}
 	void find_contours()
 	{
-		return findContours(mask, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+		findContours(mask, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
 	}
 	void do_contours(Mat background)
 	{
+		vector<vector<Point> >::iterator itc = contours.begin();
+		while (itc != contours.end())
+		{
+			if (itc->size() < 50)
+			{
+				itc = contours.erase(itc);
+			}
+			else
+			{
+				++itc;
+			}
+		}
 		drawContours(background, contours, -1, (0, 255, 0), 1);
 	}
-	void min_area()
+	void moment(Mat background)
 	{
-		rot_rect = minAreaRect(contours[0]);
+
+		imshow("mask", mask);
+		oMoments = moments(mask);
+
+		dM01 = oMoments.m01;
+		dM10 = oMoments.m10;
+		dArea = oMoments.m00;
 	}
-	void find_center()
+	void find_center(Centers color)
 	{
-		center = rot_rect.center;
+		center.x = dM10 / dArea;
+		center.y = dM01 / dArea;
 	}
 	void put_center(Mat background)
 	{
-		return putText(background, ".", center, FONT_HERSHEY_DUPLEX, 1, (0, 255, 0));
+		circle(background, center, 1, (0, 255, 0), 2);
 	}
 	void put_text(Mat background)
-	{
-		x = round(center.x * 10) / 10;
-		y = round(center.y * 10) / 10;
-		string_x = to_string(x);
-		string_y = to_string(y);
-		string_x.erase(5);
-		string_y.erase(5);
+		{
+		string_x = to_string(center.x);
+		string_y = to_string(center.y);
 		coord = "(" + string_x + "," + string_y + ")";
 		center.x += 10;
 		center.y -= 10;
@@ -74,44 +100,76 @@ public:
 
 int main()
 {
-	Scalar yellowmax = Scalar(35, 255, 255);
-	Scalar yellowmin = Scalar(23, 100, 150);
-	Scalar redmax = Scalar(184, 255, 255);
-	Scalar redmin = Scalar(170, 100, 150);
-	Scalar greenmax = Scalar(70, 255, 255);
-	Scalar greenmin = Scalar(55, 100, 150);
-	Scalar bluemax = Scalar(125, 255, 255);
-	Scalar bluemin = Scalar(95, 100, 150);
+	Scalar yellowmax = Scalar(32, 173, 255);
+	Scalar yellowmin = Scalar(0, 101, 213);
+	Scalar redmax = Scalar(179, 158, 208);
+	Scalar redmin = Scalar(153, 111, 134);
+	Scalar greenmax = Scalar(82, 146, 255);
+	Scalar greenmin = Scalar(61, 54, 132);
+	Scalar bluemax = Scalar(179, 255, 255);
+	Scalar bluemin = Scalar(95, 161, 165);
+	Scalar purplemax = Scalar(175, 83, 186);
+	Scalar purplemin = Scalar(125, 59, 151);
+	Scalar con_greenmax = Scalar(51, 108, 213);
+	Scalar con_greenmin = Scalar(37, 35, 160);
 
-	Mat target = imread("C:/Users/Volch/OneDrive/Рабочий стол/Alex/1.jpg");
-	Mat background;
-	target.copyTo(background);
-	cvtColor(target, target, COLOR_BGR2HSV);
+	VideoCapture cap(0);
 
-	Centers yellow_center, red_center, green_center, blue_center;
-	vector<Centers> centers = { yellow_center, red_center, green_center, blue_center };
+	Centers yellow_center, red_center, green_center, blue_center, purple_center;
+	vector<Centers> centers = { red_center, yellow_center, green_center, blue_center, purple_center };
 
-	Markers yellow(yellowmin, yellowmax), red(redmin,redmax), green(greenmin,greenmax), blue(bluemin, bluemax);
-	vector<Markers> mas = { yellow, red, green };
-	for (int i = 0; i < size(mas); i++)
+	vector<int> yellow_min, green_min, blue_min, purple_min;
+	int tmp = 0;
+	Markers yellow(yellowmin, yellowmax, "yellow"), red(redmin, redmax, "red"), green(greenmin, greenmax, "green"), blue(bluemin, bluemax, "blue"), purple(purplemin, purplemax, "purple"), con_green(con_greenmin, con_greenmax, "con_green");
+	vector<Markers> mas = { red, yellow, green, blue, purple };
+	
+	int k = 0;
+	int flag = 0;
+	float min_distance(1500.0);
+	while (1)
 	{
-		mas[i].do_mask(target);
-		mas[i].find_contours();
-		mas[i].do_contours(background);
-		mas[i].min_area();
-		mas[i].find_center();
-		mas[i].put_center(background);
-		mas[i].put_text(background);
-		centers[i].x = mas[i].center.x;
-		cout << centers[i].x << endl;
-		centers[i].y = mas[i].center.y;
-		cout << centers[i].y << endl;
+		flag++;
+		Mat video;
+		cap >> video;
+		imshow("Video", video);
+		Mat background;
+		float dist;
+		video.copyTo(background);
+		cvtColor(video, video, COLOR_BGR2HSV);
+		for (int i = 0; i < size(mas); i++)
+		{
+		
+			mas[i].do_mask(video);
+			mas[i].find_contours();
+			mas[i].do_contours(background);
+			mas[i].moment(background);
+			mas[i].find_center(centers[i]);
+			mas[i].put_center(background);
+			mas[i].put_text(background);
+		
+			if (i != 0)
+			{
+				dist = sqrt(pow(red.center.x - mas[i].center.x, 2) + (pow(red.center.y - mas[i].center.y, 2)));
+				if (dist < mas[i].min_dist)
+					mas[i].min_dist = dist;
+				if (dist < min_distance)
+					min_distance = dist;
+			}
+			
+		}
+		imshow("background", background);
+		char c = waitKey(33);
+		if (c == 27)
+		{
+			destroyWindow("Video");
+			break;
+		}
+
 	}
-	
-	
-	imshow("original", background);
-
+	for (int i = 1; i < size(mas); i++)
+	{
+		cout << "minimal distance by " << mas[i].name << ": " << mas[i].min_dist << endl;
+	}
 	waitKey(0);
-
 	return 0;
 }
