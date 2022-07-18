c022f77c05d644b3c0a6d62c2944e72168cf71ea
AlexeyVolchenkov <72989930+AlexeyVolchenkov@users.noreply.github.com>
2022-07-18 16:29:01 +0300

    Delete mqtt_publisher directory

--- mqtt_publisher/publisher/src/general.h
+++
@@ -1,9 +0,0 @@
-/* © 2021 AO Kaspersky Lab */
-#ifndef _PUBLISHER_GENERAL_H
-#define _PUBLISHER_GENERAL_H
-
-namespace app {
-constexpr const char *AppTag = "[Publisher] ";
-}
-
-#endif // _PUBLISHER_GENERAL_H
